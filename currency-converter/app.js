2ff7cb301d5a2bd9812908e53f3423c5c78019e1
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-11-02 22:53:04 +0530

    Update app.js
    url updated

--- currency-converter/app.js
+++ currency-converter/app.js
@@ -1,6 +1,5 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
+  "https://cdn.jsdelivr.net/npm/fawazahmed0/currency-api@1/latest/currencies";
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
